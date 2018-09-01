efeb5d2380d713db2a78bad9cd3955e29635ab67
Zsofia <zsofi.dedeo@mensa.hu>
2018-09-01 11:27:39 +0200

    feat: move Twitter keys to config.js

--- bot.js
+++ bot.js
@@ -2,13 +2,6 @@ console.log("The bot is starting");
 
 var Twit = require("twit");
 
-var T = new Twit({
-  consumer_key: process.env.TWIT_CONSUMER_KEY,
-  consumer_secret: process.env.TWIT_CONSUMER_SECRET,
-  access_token: process.env.TWIT_ACCESS_TOKEN,
-  access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true // optional - requires SSL certificates to be valid.
-});
+var config = require("./config");
 
-console.log(T);
+var T = new Twit(config);
