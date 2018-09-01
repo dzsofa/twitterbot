a0cf81af6eab910ab307a5d1c77ef996812bad65
Zsofia <zsofi.dedeo@mensa.hu>
2018-09-01 14:07:59 +0200

    feat: add post method to bot

--- bot.js
+++ bot.js
@@ -5,3 +5,30 @@ var Twit = require("twit");
 var config = require("./config");
 
 var T = new Twit(config);
+
+// var params = {
+//   q: "pizza",
+//   count: 10
+// };
+
+// function gotData(err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i = 0; i < tweets.length; i++) {
+//     console.log(tweets[i].text);
+//     console.log();
+//   }
+// }
+
+// T.get("search/tweets", params, gotData);
+
+let params = {
+  status: "I was tweeted with node.js!"
+};
+
+function postData(err, data, response) {
+  err
+    ? console.log("Yo, there's a problem")
+    : console.log("Everything is just fine");
+}
+
+T.post("statuses/update", params, postData);
