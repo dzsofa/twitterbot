7e717ef224bb4cf2892a290773c539c7ba32198a
Zsofia <zsofi.dedeo@mensa.hu>
2018-09-16 22:58:54 +0200

    refactor: move everything to separate methods in bot.js

--- bot.js
+++ bot.js
@@ -11,43 +11,38 @@ var fs = require("fs");
 
 var dirArray = ["sketches", "triangles"];
 
-setInterval(tweetMyThing, 1000 * 60 * 10);
+var directory = dirArray[Math.floor(Math.random() * dirArray.length)];
 
-//TODO - move inner function calls to separate functions
+setInterval(tweetMyThing, 1000 * 60 * 120);
+
 function tweetMyThing() {
-  var directory = dirArray[Math.floor(Math.random() * dirArray.length)];
-
-  var command =
-    process.env.PROCESSING_HOME +
-    "processing-java --sketch=" +
-    process.env.TWITTERBOT_HOME +
-    directory +
-    " --run";
+  var command = directory + "/" + directory;
 
   exec(command, processing);
+}
 
-  function processing() {
-    var fileName = directory + "/output.png";
-    var params = {
-      encoding: "base64"
-    };
-    var b64 = fs.readFileSync(fileName, params);
-    T.post("media/upload", { media_data: b64 }, uploaded);
+function processing() {
+  var fileName = directory + "/output.png";
+  var params = {
+    encoding: "base64"
+  };
+  var b64 = fs.readFileSync(fileName, params);
+  T.post("media/upload", { media_data: b64 }, uploaded);
+}
 
-    function uploaded(err, data, respsonse) {
-      var id = data.media_id_string;
-      var tweet = {
-        status: "#tweetsomesweetprocessing ",
-        media_ids: [id]
-      };
-      T.post("statuses/update", tweet, postData);
-    }
-    function postData(err, data, response) {
-      if (err) {
-        console.log("Yo, there's a problem.");
-      } else {
-        console.log("Everything's juuust fine.");
-      }
-    }
+function uploaded(err, data, respsonse) {
+  var id = data.media_id_string;
+  var tweet = {
+    status: "#tweetsomesweetprocessing ",
+    media_ids: [id]
+  };
+  T.post("statuses/update", tweet, postData);
+}
+
+function postData(err, data, response) {
+  if (err) {
+    console.log("Yo, there's a problem.");
+  } else {
+    console.log("Everything's juuust fine.");
   }
 }
